54073c32fd79c5b4dd5355aef7d489c08fe9b742
Marina <marra@inbox.ru>
2022-11-20 19:02:09 +0300

    Temp1

---
+++ Temps/Temp001/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первого.
+a = 5; b = 25 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> нет
+a = -3 b = 9 -> да
+*/
+int NumberA = new int ();
+int NumberB = new int ();
+Console.Write("Введите число1: ");
+NumberA = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число2: ");
+NumberB = Convert.ToInt32 (Console.ReadLine());
+bool result = NumberA * NumberA == NumberB;
+if (result)
+{
+    Console.WriteLine("Число " + NumberB + " является квадратом числа " + NumberA);
+}
+else
+{
+   Console.WriteLine("Число " + NumberB + " не является квадратом числа " + NumberA);
+} 
+
