d05cf549b360bbc8e9054eec1a567fdb155f1b2c
Marina <marra@inbox.ru>
2022-11-20 19:45:36 +0300

    Temp3

---
+++ Temps/Temp003/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа 
+в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"
+*/
+int N = new int();
+Console.Write ("Введите число: ");
+N = Convert.ToInt32 (Console.ReadLine());
+int number = -N;
+while ( number < N)
+{
+    Console.Write (number + ", " );
+    number = number + 1;
+}
+
+
+
+Console.WriteLine(number);
