19351f0f4d9533784557f837f3d79ef5a18eb8b2
Marina <marra@inbox.ru>
2022-11-20 15:03:18 +0300

    Example005_приветствие любимчика

---
+++ Examples/Example006_приветствие любимчика/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string name = Console.ReadLine();
+if (name.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + name);
+}
